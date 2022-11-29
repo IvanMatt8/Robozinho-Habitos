9c408d70fe56b3b224eba75a297ad2f8e70957f4
IvanMatt8 <ivanmatt1995@gmail.com>
2022-11-29 14:53:37 -0300

    Estrutura de páginas de rotas

---
+++ src/routes/AllPages.jsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Start from "../Pages/start";
+
+const Start = createNativeStackNavigator();
+
+export default function AllPages() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShow: false,
+        }}
+      >
+        <Stack.Screen name="Start" component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
